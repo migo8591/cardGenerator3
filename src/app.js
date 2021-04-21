1075a7a3b0b4aaebb4ba2ab3e74e0e21e6db4ac1
migo8591 <mcatedral24@hotmail.com>
2021-04-21 18:33:07 +0000

    segundo commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomNumberSuit = () => {
+  let suit = ["diamond", "spade", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
+};
+
+//     let suit = ["diamond", "spade", "heart", "club"];
+
+//     let indexNumbers =Math.floor(Math.random()*numbers.length);
+//     let indexSuit =Math.floor(Math.random()*suit.length);
+//     return numbers[indexNumbers];
+// }
+
+//     let numbers = ["A", "2", "3", "4", "5","6"; "7"; "8","9","10","J","Q","K"];
+
+//     let indexNumbers =Math.floor(Math.random()*numbers.length);
+
+//     return numbers[indexNumbers];
+// }
