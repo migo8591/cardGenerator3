fc5a837a917818bd7ef86cf726d1ccf9c7e752bd
migo8591 <mcatedral24@hotmail.com>
2021-04-29 04:22:33 +0000

    nuevo card Generator

--- src/app.js
+++ src/app.js
@@ -1,34 +1,34 @@
 import "./style.css";
 
-window.onload = () => {
-  document.querySelector(".card").classList.add(generateRandomSuit());
-  document.querySelector(".card").innerHTML = generateRandomNumber();
-};
+// window.onload = () => {
+//   document.querySelector(".card").classList.add(generateRandomSuit());
+//   document.querySelector(".card").innerHTML = generateRandomNumber();
+// };
 
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let indexNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[indexNumbers];
-};
-let generateRandomNumberSuit = () => {
-  let suit = ["diamond", "spade", "heart", "club"];
-  let indexSuit = Math.floor(Math.random() * suit.length);
-  return suit[indexSuit];
-};
+// let generateRandomNumber = () => {
+//   let numbers = [
+//     "A",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//     "10",
+//     "J",
+//     "Q",
+//     "K"
+//   ];
+//   let indexNumbers = Math.floor(Math.random() * numbers.length);
+//   return numbers[indexNumbers];
+// };
+// let generateRandomNumberSuit = () => {
+//   let suit = ["diamond", "spade", "heart", "club"];
+//   let indexSuit = Math.floor(Math.random() * suit.length);
+//   return suit[indexSuit];
+// };
 
 //     let suit = ["diamond", "spade", "heart", "club"];
 
