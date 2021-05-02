8ed95fc60df1fd827e38e04b539f64102ebe1b72
migo8591 <mcatedral24@hotmail.com>
2021-05-02 17:24:21 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import "./style.css";
 
 window.onload = () => {
   document.querySelector(".numbers").innerHTML = generateRandomNumber();
-  document.querySelector(".top-suit").innerHTML = generateRandomNumberSuit();
-  document.querySelector(".bottom-suit").innerHTML = generateRandomNumberSuit();
+  document.querySelector(".card").classList.add(generateRandomSuit());
 };
 
 let generateRandomNumber = () => {
@@ -25,22 +24,8 @@ let generateRandomNumber = () => {
   let indexNumbers = Math.floor(Math.random() * numbers.length);
   return numbers[indexNumbers];
 };
-let generateRandomNumberSuit = () => {
+let generateRandomSuit = () => {
   let suit = ["diamond", "spade", "heart", "club"];
   let indexSuit = Math.floor(Math.random() * suit.length);
   return suit[indexSuit];
 };
-
-//     let suit = ["diamond", "spade", "heart", "club"];
-
-//     let indexNumbers =Math.floor(Math.random()*numbers.length);
-//     let indexSuit =Math.floor(Math.random()*suit.length);
-//     return numbers[indexNumbers];
-// }
-
-//     let numbers = ["A", "2", "3", "4", "5","6"; "7"; "8","9","10","J","Q","K"];
-
-//     let indexNumbers =Math.floor(Math.random()*numbers.length);
-
-//     return numbers[indexNumbers];
-// }
